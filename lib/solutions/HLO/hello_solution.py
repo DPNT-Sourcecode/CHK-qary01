366a8a579d702eb403e02b3b34f2ee822ea8c2af
sbx_user1051 <sbx_user1051@169.254.55.149>
2024-06-10 09:20:33 +0000

    Mon Jun 10 09:20:33 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,7 +4,8 @@
 # friend_name = unicode string
 def hello(friend_name):
     if isinstance(friend_name, str):
-        return f"Hello, {friend_name.capitalize()}!"
+        return f"Hello, {friend_name.title()}!"
     
     raise ValueError("Friend name must be a string.")
+
 
