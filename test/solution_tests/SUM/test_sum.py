2cba6a12cbcb1adb302a4c4f7b79f0a0fd39bf63
sbx_user1051 <sbx_user1051@169.254.55.149>
2024-06-10 09:12:27 +0000

    Mon Jun 10 09:12:27 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,13 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_sum_non_integers(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(1, "2")
+        
+        with self.assertRaises(ValueError):
+            sum_solution.compute("1", 2)
+        
+        with self.assertRaises(ValueError):
+            sum_solution.compute("1", "2")
