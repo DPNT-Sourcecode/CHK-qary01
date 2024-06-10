61919914eca2ebe30c3d650857eac7cbd2f70d66
sbx_user1051 <sbx_user1051@169.254.55.149>
2024-06-10 09:09:27 +0000

    Mon Jun 10 09:09:27 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    return x + y
-    raise NotImplementedError()
+    if isinstance(x, int) and isinstance(y, int):
+        return x + y
+    raise ValueError("Both inputs must be integers")
+
 
